3973cf060743a74cff680fef37645f49db1f8016
KarasMI7 <steven.zhou930605@gmail.com>
2017-02-21 19:38:46 +0100

    beta Streaming .in file

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.AbstractMap;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Google!");
+        System.out.println("Hello World!");
+        System.out.println("Run for me you bitch");
+        System.out.println("Test combination is" + testCombinations(5).toString());
 
-        String file_path = "small.in";
-        stream_input(file_path);
+        String filePath = "small.in";
+        stream_input(filePath);
+    }
 
+    //starting with half row number
+    public static LinkedList testCombinations(int max){
+        LinkedList<AbstractMap.SimpleEntry> sliceSize = new LinkedList<AbstractMap.SimpleEntry>();
+        for(int i = 1; i<=max;i++) {
+            sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(1, i));
+
+        }
+        for (int j = 2; j <= max; j++) {
+            sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(j, 1));
+        }
+
+        for(int i = 1; i<=max;i++)
+            for (int j = 2; j < max; j++)
+                if(i*j <= max && i==j) sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(i,j));
+
+        return sliceSize;
     }
 
     public static void stream_input(String file_path){
@@ -23,14 +43,13 @@ public class Main {
             String line1 = br.readLine(); //reading the first line
 
             // Get four arguments (int)
-            int[] Arguments = Arrays.asList(line1.split("")).stream().mapToInt(Integer::parseInt).toArray();
+            int[] Arguments = Arrays.asList(line1.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
 
             System.out.println("line1 is here  " +line1);
             while((line = br.readLine()) != null){
                 char [] charTemp =line.toCharArray();
 
                 for (int i = 0; i<charTemp.length; i++){
-
                 }
             }
 
@@ -41,4 +60,5 @@ public class Main {
         }
 
     }
+
 }
