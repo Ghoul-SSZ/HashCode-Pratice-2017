c625adff42775defe993b1a479d793881807a0b5
Nick <namp@kth.se>
2017-02-21 19:23:29 +0100

    Test combinations

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import java.util.AbstractMap;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
         System.out.println("Run for me you bitch");
+        System.out.println("Test combination is" + testCombinations(5).toString());
     }
 
-    public void testCombinations(int max){
+    //starting with half row number
+    public static LinkedList testCombinations(int max){
+        LinkedList<AbstractMap.SimpleEntry> sliceSize = new LinkedList<AbstractMap.SimpleEntry>();
+        for(int i = 1; i<max;i++) {
+            sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(1, i));
 
+        }
+        for (int j = 2; j < max; j++) {
+            sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(j, 1));
+        }
+
+        for(int i = 1; i<max;i++)
+            for (int j = 2; j < max; j++)
+                if(i*j <= max && i==j) sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(i,j));
+
+        return sliceSize;
     }
 }
