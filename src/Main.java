bf27e7353ed326aca01098a3a15d8cbacb105600
KarasMI7 <steven.zhou930605@gmail.com>
2017-02-21 19:32:05 +0100

    Merge branch 'master' of /Users/stevenzhou/Documents/HashCode-Pratice-2017 with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,44 @@
-import java.util.AbstractMap;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println("Run for me you bitch");
-        System.out.println("Test combination is" + testCombinations(5).toString());
+        System.out.println("Hello Google!");
+
+        String file_path = "small.in";
+        stream_input(file_path);
+
     }
 
-    //starting with half row number
-    public static LinkedList testCombinations(int max){
-        LinkedList<AbstractMap.SimpleEntry> sliceSize = new LinkedList<AbstractMap.SimpleEntry>();
-        for(int i = 1; i<=max;i++) {
-            sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(1, i));
+    public static void stream_input(String file_path){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file_path));
+            String line;
+            String line1 = br.readLine(); //reading the first line
 
+            // Get four arguments (int)
+            int[] Arguments = Arrays.asList(line1.split("")).stream().mapToInt(Integer::parseInt).toArray();
+
+            System.out.println("line1 is here  " +line1);
+            while((line = br.readLine()) != null){
+                char [] charTemp =line.toCharArray();
+
+                for (int i = 0; i<charTemp.length; i++){
+
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        for (int j = 2; j <= max; j++) {
-            sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(j, 1));
-        }
 
-        for(int i = 1; i<=max;i++)
-            for (int j = 2; j < max; j++)
-                if(i*j <= max && i==j) sliceSize.add(new AbstractMap.SimpleEntry<Integer, Integer>(i,j));
-
-        return sliceSize;
     }
 }
