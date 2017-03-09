2040949bdf06b267891d0afbc3dea442a46e0f1e
KarasMI7 <steven.zhou930605@gmail.com>
2017-03-09 20:04:33 +0100

    Bugs needs fix

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static ArrayList<ArrayList<Character>> twoDMap = new ArrayList<>();
+    static HashMap<ArrayList<HashMap<Integer,Integer>>,Integer> grouping = new HashMap<>();
+    public static int groupNumber=1;
+
     public static void main(String[] args) {
 
-        System.out.println("Hello HashCode!  :D ");
+        System.out.println("Hello HashCode! :D ");
         System.out.println("Test combination is" + testCombinations(5).toString());
 
         //import input files
@@ -19,17 +24,62 @@ public class Main {
 
         //information setup
         Scanner in = new Scanner(System.in);
-        //How many Tomato must be in one slice of pizza
+        //How many Tomato&Mushroom must be in one slice of pizza
         //System.out.println();
-        int t = 1;
+        int tAndm = 1;
 
-        //How many Mushroom must be in one slice of pizza
+        //Max cell for one slice of pizza
         //System.out.println();
-        int m = 1;
+        int max = 5;
 
-        //main process
-        HashMap<Integer,Integer> request = new HashMap<>();
-        System.out.println(request.isEmpty());
+        LinkedList combs = testCombinations(max);
+
+
+            //main process
+            for (int i=0; i<twoDMap.size(); i++){
+                for (int j=0; j<twoDMap.get(i).size(); j++){
+                    Object cell = twoDMap.get(i).get(j);
+                    if (cell.equals("T") || cell.equals("M")){
+                        System.out.println("cell taken");
+                    }else{
+                        //starting testing combinations
+                        ArrayList<Character> testGroup = new ArrayList();
+                        for (int k=0; k<combs.size(); k++){
+                            //test the combination
+                            getCombination(combs.get(k),testGroup,0,0);
+                        }
+                    }
+                }
+            }
+        System.out.println(twoDMap);
+    }
+
+
+
+    public static void getCombination(Object combs, ArrayList testGroup,int row, int column){
+        String t = combs.toString();
+        Pattern pattern = Pattern.compile("= *");
+        Matcher matcher = pattern.matcher(t);
+        if (matcher.find()) {
+            String t1 = t.substring(0, matcher.start());
+            String t2 = t.substring(matcher.end());
+            System.out.println("t1: " +t1);
+             row = Integer.parseInt(t1);
+             column = Integer.parseInt(t2);
+        }
+        for (int i=0; i<row;i++){
+            for (int j=0;j<column;j++){
+                testGroup.add(twoDMap.get(i).get(j));
+            }
+        }
+        if (statusCheck(1,testGroup)){
+            for (int i=0; i<row;i++){
+                for (int j=0;j<column;j++){
+                    twoDMap.get(i).set(j, ((char) groupNumber));
+                }
+            }
+            grouping.put(testGroup,testGroup.size());
+        }
 
     }
 
@@ -62,7 +112,7 @@ public class Main {
             // Get four arguments (int)
             int[] Arguments = Arrays.asList(line1.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
 
-            System.out.println("line1 is here  " +line1);
+            System.out.println("arguments is here:  " +line1);
             while((line = br.readLine()) != null){
                 char [] charTemp =line.toCharArray();
                 ArrayList<Character> cAlist = new ArrayList<Character>() ;
@@ -82,20 +132,20 @@ public class Main {
     }
 
     //Checking if the pizza slice already fulfilled the requirement
-    public boolean statusCheck (int tomato, int mushroom, ArrayList<Character> testing){
+    public static boolean statusCheck (int tAndM, ArrayList<Character> testing){
         int tomatoCount=0;
         int mushroomCount=0;
         for (int i=0; i<testing.size();i++) {
-            if (testing.get(i).toString() == "T") {
+            if (testing.get(i).equals("T")) {
                 tomatoCount++;
-            } else if (testing.get(i).toString() == "M") {
+            } else if (testing.get(i).equals("M")) {
                 mushroomCount++;
             } else {
                 System.out.println("Dude, something is wrong!");
             }
         }
 
-        if (tomatoCount >= tomato && mushroom >= mushroomCount){
+        if (tomatoCount >= tAndM && mushroomCount >= tAndM){
             return true;
         }else{return false;}
 
